eb043d3b04bd7b464ce2d5e346058f7a965d0309
aasscc07 <75631159+aasscc07@users.noreply.github.com>
2022-04-17 00:49:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,68 +6,67 @@ import hashlib
 import base64
 import pickle
 
-
-
-
-
-
-        
-
+MPath = "folder"
 
 Kinput = input("암호화 : E / 복호화 : D\n : ")
 if Kinput == "E":
     FileStr = ""
-    for Path in os.listdir("folder"):
+    for Path in os.listdir(MPath):
         try:
-            with open(os.path.join("folder",Path),'rb') as f:
+            with open(os.path.join(MPath,Path),'rb') as f:
                 FileStr = f.read().decode('utf-8')
                 f.close()
         except:
-            with open(os.path.join("folder",Path),'rb') as f:
+            with open(os.path.join(MPath,Path),'rb') as f:
                 FileStr = f.read()#.decode('ascii')
                 f.close()
             
 
         try:               
-            with open(os.path.join("folder",Path),'wb') as f:
+            with open(str(os.path.join(MPath,Path) + ".jun"),'wb') as f:
                 FileStr_base64 = FileStr.encode('ascii')
                 FileStr_base64 = base64.b64encode(FileStr_base64)
                 FileStr = FileStr_base64.decode('ascii')
                 f.write(FileStr.encode('utf-8'))
                 f.close()
         except:
-            with open(os.path.join("folder",Path),'wb') as f:
+            with open(str(os.path.join(MPath,Path) + ".jun"),'wb') as f:
                 FileStr_base64 = FileStr
                 FileStr_base64 = base64.b64encode(FileStr_base64)
                 FileStr = FileStr_base64
                 f.write(FileStr)
                 f.close()
+        
+        os.remove(os.path.join(MPath,Path))
 
 
 elif Kinput == "D":
     FileStr = ""
-    for Path in os.listdir("folder"):
+    for Path in os.listdir(MPath):
         try:
-            with open(os.path.join("folder",Path),'rb') as f:
+            with open(os.path.join(MPath,Path),'rb') as f:
                 FileStr = f.read().decode('utf-8')
                 f.close()
         except:
-            with open(os.path.join("folder",Path),'rb') as f:
+            with open(os.path.join(MPath,Path),'rb') as f:
                 FileStr = f.read()
                 f.close()
             
         try:
-            with open(os.path.join("folder",Path),'wb') as f:
+            with open(os.path.join(MPath,Path).split(".jun")[0],'wb') as f:
                 FileStr_base64 = base64.b64decode(FileStr)
                 FileStr = FileStr_base64.decode('ascii')
                 f.write(FileStr.encode('utf-8'))
                 f.close()
+                
         except:
-            with open(os.path.join("folder",Path),'wb') as f:
+            with open(os.path.join(MPath,Path).split(".jun")[0],'wb') as f:
                 FileStr_base64 = base64.b64decode(FileStr)
                 FileStr = FileStr_base64
                 f.write(FileStr)
                 f.close()
+        
+        os.remove(os.path.join(MPath,Path))
 
 else:
     print("키보드 입력이 잘못되었습니다")
